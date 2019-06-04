df44fac395cdb558efd3ea208edf87432895d501
Victor Abrell <victor@abrell.se>
2019-06-04 07:25:24 +0200

    changed to swe text

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -52,7 +52,7 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        {{ __('Kom ihåg mig') }}
                                     </label>
                                 </div>
                             </div>
