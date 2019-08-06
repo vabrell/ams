dba671758ba30f519d3254ed4d4f2211801667d1
Victor Abrell <victor@abrell.se>
2019-08-06 08:05:09 +0200

    Refactoring jquery document ready

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,96 +6,57 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+$(function(){
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+    $("#btnAdd").click(function(){
+        var selectedOpt = $("#noRole option:selected");
 
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.'){0], files(key).default));
+        selectedOpt.addClass('moved');
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+        if(selectedOpt.length === 0) return;
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app',
-});
-
-$("#btnAdd").click(function(){
-    var selectedOpt = $("#noRole option:selected");
-
-    selectedOpt.addClass('moved');
-
-    if(selectedOpt.length === 0) return;
-
-    $("#contract").append($(selectedOpt).clone());
-    $(selectedOpt).remove();
-});
-
-$("#btnRemove").click(function(){
-    var selectedOpt = $("#contract option:selected");
-
-    selectedOpt.addClass('moved');
-
-    if(selectedOpt.length === 0) return;
-
-    $("#noRole").append($(selectedOpt).clone());
-    $(selectedOpt).remove();
-});
-
-$("#roleSave").click(function(e){
-    $("#contract option.moved").prop('selected', true);
-    $("#noRole option.moved").prop('selected', true);
-});
-
-function appendLeadingZero(n)
-{
-    if(n <= 9)
-    {
-        return "0" + n;
-    }
-    return n;
-}
-
-function getDate(datestamp){
-    var date = new Date(datestamp);
-    var d = appendLeadingZero(date.getDate());
-    var m = appendLeadingZero(date.getMonth() + 1);
-    var y = date.getFullYear();
-
-    return y + "-" + m + "-" + d;
-}
-
-if($("#startDate").val())
-{
-    var date = $("#startDate").val();
-
-    var newDate = new Date(date);
-    var minDate = getDate(newDate.setDate(newDate.getDate() + 1));
-    var maxDate = getDate(newDate.setDate(newDate.getDate() + 33));
-
-    $("#endDate").attr({
-        'disabled': false,
-        'min': minDate,
-        'max': maxDate,
+        $("#contract").append($(selectedOpt).clone());
+        $(selectedOpt).remove();
     });
-}
 
-$("#startDate").change(function(){
-    var date = $("#startDate").val();
+    $("#btnRemove").click(function(){
+        var selectedOpt = $("#contract option:selected");
 
-    if(date)
+        selectedOpt.addClass('moved');
+
+        if(selectedOpt.length === 0) return;
+
+        $("#noRole").append($(selectedOpt).clone());
+        $(selectedOpt).remove();
+    });
+
+    $("#roleSave").click(function(e){
+        $("#contract option.moved").prop('selected', true);
+        $("#noRole option.moved").prop('selected', true);
+    });
+
+    function appendLeadingZero(n)
     {
+        if(n <= 9)
+        {
+            return "0" + n;
+        }
+        return n;
+    }
+
+    function getDate(datestamp){
+        var date = new Date(datestamp);
+        var d = appendLeadingZero(date.getDate());
+        var m = appendLeadingZero(date.getMonth() + 1);
+        var y = date.getFullYear();
+
+        return y + "-" + m + "-" + d;
+    }
+
+    if($("#startDate").val())
+    {
+        var date = $("#startDate").val();
+
         var newDate = new Date(date);
         var minDate = getDate(newDate.setDate(newDate.getDate() + 1));
         var maxDate = getDate(newDate.setDate(newDate.getDate() + 33));
@@ -106,12 +67,30 @@ $("#startDate").change(function(){
             'max': maxDate,
         });
     }
-    else
-    {
-        $("#endDate").attr({
-            'disabled': true,
-            'min': '',
-            'max': '',
-        }).val('');
-    }
+
+    $("#startDate").change(function(){
+        var date = $("#startDate").val();
+
+        if(date)
+        {
+            var newDate = new Date(date);
+            var minDate = getDate(newDate.setDate(newDate.getDate() + 1));
+            var maxDate = getDate(newDate.setDate(newDate.getDate() + 33));
+
+            $("#endDate").attr({
+                'disabled': false,
+                'min': minDate,
+                'max': maxDate,
+            });
+        }
+        else
+        {
+            $("#endDate").attr({
+                'disabled': true,
+                'min': '',
+                'max': '',
+            }).val('');
+        }
+    });
+
 });
